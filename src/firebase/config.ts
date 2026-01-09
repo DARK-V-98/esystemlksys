15f4413cbd58db42688ec2fcbaad53a10e39605a
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2026-01-09 18:25:53 +0000

    LIBK TO THIS APP // Import the functions you need from the SDKs you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,16 @@ import { initializeApp, getApps } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyClqbfmL5kFN1gfUfel06tAJaiydLgvCL8",
+  authDomain: "esystemlk.firebaseapp.com",
+  databaseURL: "https://esystemlk-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "esystemlk",
+  storageBucket: "esystemlk.firebasestorage.app",
+  messagingSenderId: "520998716067",
+  appId: "1:520998716067:web:8a24e2cd2cb581bb687516",
+  measurementId: "G-MQ9S4TKT2E"
 };
 
 // Initialize Firebase
@@ -20,6 +23,5 @@ if (!getApps().length) {
 } else {
   app = getApps()[0];
 }
-
 
 export { app };
